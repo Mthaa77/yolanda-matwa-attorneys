057832159917c14ea701301df67394ac0ab1d411
Mthaa77 <tshepisokadiaka83@gmail.com>
2026-07-06 11:22:25 +0200

    Add references for uploaded logo and practice imagery

--- src/lib/director-welcome-image.ts
+++ src/lib/director-welcome-image.ts
@@ -2,3 +2,5 @@
 export const directorWelcomeImage = "/images/Yolanda_Matwa_Attorneys_legal_needs_2026.jpeg";
 export const constitutionFeatureImage = "/images/Constitution_of_South_Africa_poster_202607060703.jpeg";
 export const justiceFeatureImage = "/images/1698671809880.jpeg";
+export const firmLogoImage = "/images/Yolanda matwa attorneys logo.png";
+export const practiceConversationImage = "/images/pexels-sora-shimazaki-5669619-scaled.jpg";
